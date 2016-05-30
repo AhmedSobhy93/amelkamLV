906d3b931c160618a36749d4320c025b4ad70c92
ahmed sobhy <eng_ahmed9393@yahoo.com>
2016-05-30 15:33:53 +0200

    Sign-up & Login Done ** without user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  attr_accessible :name,:email,:dob,:phone,:password,:gender
+
+  validates :phone, presence: true, length: {minimum:10 , maximum: 11 }
+  validates :name, presence: true, length: {minimum: 6, maximum: 30 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :prices
   resources :products
   resources :comments
   resources :rates
   resources :categories
   devise_for :users
+
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
