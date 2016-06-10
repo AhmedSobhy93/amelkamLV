1fa5e673c291526797b54ced5a29e691027a6f32
ahmed sobhy <eng_ahmed9393@yahoo.com>
2016-06-10 03:38:40 +0200

    Search by product name and category name

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,20 @@
 class SearchController < ApplicationController
   def index
-  	@products_search = Product.search(params[:name]).all
+  	cat_id = params[:product][:category_id]
+  	product_name = params[:name]
+
+  	#@products_search = Product.search(params[:name]).where(:category_id => :cat_id).all
+  	#@products_search=Product.where(:category_id => cat_id AND ).where(['name LIKE ?', "%#{product_name}%"])
+
+  	if cat_id.blank? or product_name.blank?
+  		@products_search=nil
+  	else
+  		@products_search = Product.where("name LIKE :name1 AND category_id = :category_id1",
+  {:name1 => "#{product_name}%", :category_id1 => cat_id}).all
+  	end	
 
   	respond_to do |format|
-      format.js
+      		format.js 
     end
   end
 end
